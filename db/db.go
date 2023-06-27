4d90953203c272ec144a49c02970e272583ea98a
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-27 21:29:35 +0900

    Go_env追記

--- db/db.go
+++ db/db.go
@@ -11,12 +11,12 @@ import (
 )
 
 func NewDB() *gorm.DB {
-	// if os.Getenv("GO_ENV") == "dev" {
+	if os.Getenv("GO_ENV") == "dev" {
 		err := godotenv.Load()
 		if err != nil {
 			log.Fatalln(err)
 		}
-	// }
+	}
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
